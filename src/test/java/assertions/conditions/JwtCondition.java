d26a1f49286f538643e896133f50738dbe6d8076
Alex <udinkik@mail.ru>
2025-07-10 23:23:22 +0300

    add parallel cases

--- src/test/java/assertions/conditions/JwtCondition.java
+++ src/test/java/assertions/conditions/JwtCondition.java
@@ -2,12 +2,12 @@ package assertions.conditions;
 
 import assertions.Condition;
 import io.restassured.response.ValidatableResponse;
-import org.junit.jupiter.api.Assertions;
+import org.testng.Assert;
 
 public class JwtCondition implements Condition {
     @Override
     public void check(ValidatableResponse response) {
         String jwt = response.extract().jsonPath().getString("token");
-        Assertions.assertNotNull(jwt);
+        Assert.assertNotNull(jwt);
     }
 }
