f59930849c6afbf8b9258507904bb5bd730ada2d
Alex <udinkik@mail.ru>
2025-07-12 19:22:32 +0300

    add Owner

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,18 +1,21 @@
 package tests;
 
+import config.ApiConfig;
 import io.restassured.RestAssured;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import listeners.CustomTpl;
+import org.aeonbits.owner.ConfigFactory;
 import org.testng.annotations.BeforeSuite;
 import services.UserService;
 
 public class BaseTest {
     protected final UserService userService = new UserService();
+    protected static ApiConfig config = ConfigFactory.create(ApiConfig.class);
 
     @BeforeSuite
     public static void setUp() {
-        RestAssured.baseURI = "http://85.192.34.140:8080/api";
+        RestAssured.baseURI = config.baseUrl();
 
         RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter(),
                 CustomTpl.customLogFilter().withCustomTemplates());
